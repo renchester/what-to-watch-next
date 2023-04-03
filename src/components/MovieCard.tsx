94ce09ad30dfbe576fde445ae1d381f8658dab92
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-04 07:23:13 +0800

    Add MovieCard component

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,5 +1,7 @@
+import { Link } from 'react-router-dom';
 import getYear from 'date-fns/getYear';
-import { Movie } from '../types/types';
+
+import { Movie, PosterSizes } from '../types/types';
 
 type MovieCardProps = {
   movie: Movie;
@@ -8,30 +10,32 @@ type MovieCardProps = {
 function MovieCard(props: MovieCardProps) {
   const { movie } = props;
   const IMG_URL = import.meta.env.VITE_TMDB_IMG_URL;
-  const IMG_SIZE_IDENTIFIER = '/w342';
+  const IMG_SIZE_IDENTIFIER: PosterSizes = 'w342';
 
   return (
-    <article className="mv-card" aria-label="movie card">
-      {movie.poster_path && (
-        <img
-          src={`${IMG_URL}${IMG_SIZE_IDENTIFIER}${movie.poster_path}`}
-          alt={`Poster for ${movie.title || 'this movie'}`}
-          className="mv-card__img"
-        />
-      )}
-      <h3 className="mv-card__title" aria-label="movie title">
-        {movie.title}
-      </h3>
-      <h4 className="mv-card__year" aria-label="movie release year">
-        {getYear(new Date(movie.release_date || 1))}
-      </h4>
-      <p
-        className="mv-card__overview"
-        aria-label={`Plot overview for ${movie.title || 'this movie'}`}
-      >
-        {movie.overview}
-      </p>
-    </article>
+    <Link to={`/movie/${movie.id}`}>
+      <article className="mv-card" aria-label="movie card">
+        {movie.poster_path && (
+          <img
+            src={`${IMG_URL}/${IMG_SIZE_IDENTIFIER}${movie.poster_path}`}
+            alt={`Poster for ${movie.title || 'this movie'}`}
+            className="mv-card__img"
+          />
+        )}
+        <h3 className="mv-card__title" aria-label="movie title">
+          {movie.title}
+        </h3>
+        <h4 className="mv-card__year" aria-label="movie release year">
+          {getYear(new Date(movie.release_date || 1))}
+        </h4>
+        <p
+          className="mv-card__overview"
+          aria-label={`Plot overview for ${movie.title || 'this movie'}`}
+        >
+          {movie.overview}
+        </p>
+      </article>
+    </Link>
   );
 }
 export default MovieCard;
