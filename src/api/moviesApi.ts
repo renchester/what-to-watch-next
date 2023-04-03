8c8f444f1a204e04f518c6c448d5e93d970ede59
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-04 07:15:05 +0800

    Generate additional fetch functions for item search and details

--- src/api/moviesApi.ts
+++ src/api/moviesApi.ts
@@ -1,5 +1,10 @@
 import axios, { AxiosResponse } from 'axios';
-import { Movie, MovieCategory } from '../types/types';
+import {
+  Movie,
+  MovieCategory,
+  MovieDetailsType,
+  MovieCredits,
+} from '../types/types';
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const API_URL = import.meta.env.VITE_TMDB_API_URL;
@@ -15,11 +20,60 @@ type Data = {
 
 export const fetchMovies = async (category: MovieCategory) => {
   const response: AxiosResponse = await axios.get(
-    `${API_URL}/${category}${endpoint}`,
+    `${API_URL}/movie/${category}${endpoint}`,
   );
   const data = response.data as Data;
 
   if (!data || response.status !== 200) throw new Error('Unable to fetch data');
 
   return data.results;
+};
+
+export const fetchMovieQueryResults = async (query: string) => {
+  const response: AxiosResponse = await axios.get(
+    `${API_URL}/search/movie${endpoint}&query=${query}&include_adult=false`,
+  );
+  const data = response.data as Data;
+
+  if (!data || response.status !== 200)
+    throw new Error('Unable to fetch search results');
+
+  return data.results;
+};
+
+export const fetchMovieDetails = async (id: string) => {
+  const response: AxiosResponse = await axios.get(
+    `${API_URL}/movie/${id}${endpoint}`,
+  );
+  const data = response.data as MovieDetailsType;
+
+  if (!data || response.status !== 200)
+    throw new Error('Unable to fetch movie details');
+
+  return data;
+};
+
+export const fetchMovieRecommendations = async (id: string) => {
+  const response: AxiosResponse = await axios.get(
+    `${API_URL}/movie/${id}/recommendations${endpoint}`,
+  );
+  const data = response.data as Data;
+
+  if (!data || response.status !== 200)
+    throw new Error('Unable to fetch movie recommendations');
+
+  return data.results;
+};
+
+export const fetchMovieCast = async (id: string) => {
+  const response: AxiosResponse = await axios.get(
+    `${API_URL}/movie/${id}/credits${endpoint}`,
+  );
+
+  const data = response.data as MovieCredits;
+
+  if (!data || response.status !== 200)
+    throw new Error('Unable to fetch movie credits');
+
+  return data;
 };
