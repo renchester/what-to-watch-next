054e74a57509b10d4e61647f8f39b5642c5df5e9
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-05 05:44:46 +0800

    Add WatchlistSection component

---
+++ src/components/WatchlistSection.tsx
@@ -0,0 +1,29 @@
+import { Suspense } from 'react';
+import { nanoid } from 'nanoid';
+import { Watchlist } from '../types/types';
+import WatchlistCard from './WatchlistCard';
+import Loading from './Loading';
+
+type WatchlistSectionProps = {
+  list: Watchlist;
+};
+
+function WatchlistSection(props: WatchlistSectionProps) {
+  const { list } = props;
+
+  return (
+    <section className="wl-section" aria-label={`Watchlist section`}>
+      <h1 className="wl-section__title" aria-label="watchlist title">
+        All your movies
+      </h1>
+
+      <ul className="wl-section__list">
+        <Suspense fallback={<Loading message="Fetching watchlist details" />}>
+          {list.length >= 1 &&
+            list.map((id) => <WatchlistCard movieId={id} key={nanoid()} />)}
+        </Suspense>
+      </ul>
+    </section>
+  );
+}
+export default WatchlistSection;
