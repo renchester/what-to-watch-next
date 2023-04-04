c7b05ccee93b1da79f120062aaf04d5dc1a90235
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-05 05:45:36 +0800

    Add WatchlistProvider wrapper in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Outlet } from 'react-router-dom';
 
+import { WatchlistProvider } from './context/WatchlistContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
+    <WatchlistProvider>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </WatchlistProvider>
   );
 }
 export default App;
