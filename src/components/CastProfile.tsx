eb18f27b9dfa647e4ea0f80075b5d48a01edaa25
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-06 06:59:07 +0800

    Add placeholder images for empty image paths

--- src/components/CastProfile.tsx
+++ src/components/CastProfile.tsx
@@ -1,4 +1,5 @@
 import { MovieCast, MovieCrew, ProfileSizes } from '../types/types';
+import placeholder from '../assets/img/placeholder-profile.png';
 
 type CastProfileProps = {
   type: 'cast' | 'crew';
@@ -13,11 +14,14 @@ function CastProfile(props: CastProfileProps) {
 
   return (
     <li className="cast-prof" aria-label={`${type} profile`}>
-      {profile.profile_path && (
+      {profile.profile_path ? (
         <img
           src={`${IMG_URL}/${IMG_SIZE_IDENTIFIER}${profile.profile_path}`}
           alt={`Profile for ${profile.name || 'a cast/crew member'}`}
+          className="cast-prof__img"
         />
+      ) : (
+        <img src={placeholder} alt="Empty profile" className="cast-prof__img" />
       )}
 
       <h4 className="cast-prof__name">{profile.name}</h4>
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import getYear from 'date-fns/getYear';
 
 import { Movie, PosterSizes } from '../types/types';
+import placeholderPortrait from '../assets/img/placeholder-portrait.png';
 
 type MovieCardProps = {
   movie: Movie;
@@ -9,16 +10,24 @@ type MovieCardProps = {
 
 function MovieCard(props: MovieCardProps) {
   const { movie } = props;
+
   const IMG_URL = import.meta.env.VITE_TMDB_IMG_URL;
   const IMG_SIZE_IDENTIFIER: PosterSizes = 'w342';
 
   return (
-    <Link to={`/movie/${movie.id}`}>
+    <Link to={`/movie/${movie.id}`} className="mv-card__link" role="button">
       <article className="mv-card" aria-label="movie card">
-        {movie.poster_path && (
+        {movie.poster_path ? (
           <img
+            loading="lazy"
             src={`${IMG_URL}/${IMG_SIZE_IDENTIFIER}${movie.poster_path}`}
             alt={`Poster for ${movie.title || 'this movie'}`}
+            className="mv-card__img"
+          />
+        ) : (
+          <img
+            src={placeholderPortrait}
+            alt={`No image available for ${movie.title || 'this movie'} `}
             className="mv-card__img"
           />
         )}
@@ -26,20 +35,25 @@ function MovieCard(props: MovieCardProps) {
           {movie.title}
         </h3>
         {movie.release_date && (
-          <h4 className="mv-card__year" aria-label="movie release year">
+          <h4
+            className="mv-card__year"
+            aria-label="movie release year and rating"
+          >
             {getYear(new Date(movie.release_date))}
+            {!!movie.vote_average && ` • ${movie.vote_average.toFixed(1)}/10`}
           </h4>
         )}
-        {movie.overview && (
-          <p
-            className="mv-card__overview"
-            aria-label={`Plot overview for ${movie.title || 'this movie'}`}
-            data-testid="mv-card__overview"
-          >
-            {movie.overview}
-          </p>
-        )}
       </article>
+
+      {movie.overview && (
+        <p
+          className="mv-card__overview"
+          aria-label={`Plot overview for ${movie.title || 'this movie'}`}
+          data-testid="mv-card__overview"
+        >
+          {movie.overview}
+        </p>
+      )}
     </Link>
   );
 }
