ea79e662780a88c84f8019fae79ffaf11e75020b
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-06 06:59:38 +0800

    Add EmptySearch component

--- src/components/EmptySearch.tsx
+++ src/components/EmptySearch.tsx
@@ -1,4 +1,29 @@
-function EmptySearch() {
-  return <div>Get search now</div>;
+import { useNavigate } from 'react-router-dom';
+
+type EmptySearchProps = {
+  query?: string;
+};
+
+function EmptySearch(props: EmptySearchProps) {
+  const { query } = props;
+  const navigate = useNavigate();
+
+  const goToHome = () => {
+    navigate('/');
+  };
+
+  return (
+    <>
+      <h1 className="empty-res__title">
+        Your search {query && `for ${query}`} does not match anything in our
+        database
+      </h1>
+      <h2 className="empty-res__subtitle">Try searching for something else</h2>
+
+      <button type="button" className="empty-res__btn" onClick={goToHome}>
+        Go back to homepage
+      </button>
+    </>
+  );
 }
 export default EmptySearch;
